41f62345aa6bbff2459f8bceb2f15f0c6b534828
Student <example@example.com>
2019-10-23 15:27:01 -0700

    Add new functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
   $("form#appt-form").submit(function(event) {
     var name = $("input#name").val();
-    var color = $("input#color").val();
+    var color = $("#color").val();
     var desc = $("input#desc").val();
-    var date = $("input#date").val();
+    var date = $("#date").val();
     var stend = $("input#stend").val();
+    var beverage = $("select#beverage").val();
+    var flavor = $("input:radio[name=flavor]:checked").val();
 
     $(".name").text(name);
     $(".color").text(color);
     $(".desc").text(desc);
     $(".date").text(date);
     $(".stend").text(stend);
-
+    $(".beverage").text(beverage);
     $("#appointment").show();
+    $(".flavor").text(flavor);
     event.preventDefault();
   });
 });
